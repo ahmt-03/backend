79125e45b45b3456c1b5854ffeba4787f3e45da3
taymoor03 <taimoorahmad039@gmail.com>
2022-09-01 22:37:16 +0200

    back to default changing

--- index.js
+++ index.js
@@ -14,17 +14,12 @@ const microsoftresearchRouter = require('./routes/microsoftresearch');
 const timeRouter = require('./routes/time');
 const protectRoute = require('./middlewares/protectRouteMiddleware');
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: true }));
 app.use('/api/auth', authRouter);
 app.use('/api/paperswithcode', protectRoute, paperswithcodeRouter);
 app.use('/api/zenodo', protectRoute, zenodoRouter);
